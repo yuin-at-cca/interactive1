67a19e1b13c32dd3547409d84f9b2db1253d1e01
Yuin Chien <tinyuin.chien@gmail.com>
2019-09-25 14:48:23 -0700

    Added shuffling & colors

--- ASCII-town/assets/script.js
+++ ASCII-town/assets/script.js
@@ -1,37 +1,32 @@
-var colors = ['#E74C3C', '#2ECC71', '#F4D03F', '#16A085', '#F39C12', '#8E44AD'];
+var colors = ['#E74C3C', '#8E44AD', '#3498DB', '#1ABC9C', '#27AE60', '#F39C12', '#34495E', '#626567'];
+var backgrounds = ['#FDEDEC', '#F4ECF7', '#EBF5FB', '#E8F8F5', '#E9F7EF', '#FEF5E7', '#EBEDEF', '#F8F9F9'];
 
 document.addEventListener('DOMContentLoaded', function(){
-  // var objDate = new Date();
-  // var hours = objDate.getHours();
-  // console.log(hours);
-  // if(hours > 7 && hours < 18){
-  //       $('body').addClass('day');
-  // }
-  // else{
-  //       $('body').addClass('night');
-  // }
-  // if(hours == 6 || hours == 18){
-  //       $('body').addClass('dusk');
-  // }
+
+  var colorIndex = Math.floor( Math.random()*colors.length );
+  var color = colors[colorIndex];
+  var bg = backgrounds[colorIndex];
+
+  document.body.style.background = bg;
+
+  var links = document.querySelectorAll('a');
+  links.forEach(function(elem){
+    elem.style.color = color;
+  });
+
+  var town = document.getElementById('town');
+  var houses = document.querySelectorAll('.house');
+
+  for(var i = town.children.length; i >= 0; i--) {
+    town.appendChild(town.children[Math.random() * i | 0]);
+  }
 
   // rearrange order of houses on load
-  var elem = document.querySelector('.grid');
+  var elem = document.querySelector('#town');
   var pckry = new Packery( elem, {
     // options
     itemSelector: '.house',
-    gutter: 20
+    gutter: 10
   });
 
-
-  // for(var i = 0; i < houses.length; i++){
-  //     var target = Math.floor(Math.random() * houses.length -1) + 1;
-  //     var target2 = Math.floor(Math.random() * houses.length -1) +1;
-  //     houses.eq(target).before(houses.eq(target2));
-  // }
-  //
-  // // fit houses together
-  // $('#town').packery({
-  //   itemSelector: '.house',
-  //   gutter: 20
-  // });
 });
